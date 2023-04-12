38eb43fc08e5645dbbadd9e783aff905fb723e3e
Icaro <luizicaro.s@gmail.com>
2023-04-12 14:59:24 -0300

    Implement Footer component

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,8 +1,12 @@
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 import React from 'react';
 
 export default function Homepage() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
