6a084d63b3daa04c4504aaccc3d557fe39ac54d2
Icaro <luizicaro.s@gmail.com>
2023-04-12 16:19:51 -0300

    Implement Title component

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,6 +1,7 @@
 import Banner from 'components/Banner';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
+import Title from 'components/Title';
 import React from 'react';
 
 export default function Homepage() {
@@ -8,6 +9,7 @@ export default function Homepage() {
     <>
       <Header />
       <Banner image="home" />
+      <Title><h1>Um lugar para guardar seus vídeos e filmes!</h1></Title>
       <Footer />
     </>
   );
